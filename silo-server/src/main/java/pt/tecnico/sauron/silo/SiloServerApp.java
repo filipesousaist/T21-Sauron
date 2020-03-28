83dd5182dfc980ed1acdecaf13e8fd326a266c16
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2020-03-28 19:57:34 +0000

    Initilized silo server.

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
@@ -1,9 +1,15 @@
 package pt.tecnico.sauron.silo;
 
 
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class SiloServerApp {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println(SiloServerApp.class.getSimpleName());
 		
 		// receive and print arguments
@@ -11,6 +17,18 @@ public class SiloServerApp {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+
+		int port = Integer.parseInt(args[0]);
+
+		BindableService service = new SiloServerImpl();
+
+		Server server = ServerBuilder.forPort(port).addService(service).build();
+
+		server.start();
+
+		server.awaitTermination();
 	}
+
+
 	
 }
---
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
@@ -0,0 +1,7 @@
+package pt.tecnico.sauron.silo;
+
+import pt.tecnico.sauron.silo.grpc.SiloServiceGrpc;
+
+public class SiloServerImpl extends SiloServiceGrpc.SiloServiceImplBase {
+
+}
