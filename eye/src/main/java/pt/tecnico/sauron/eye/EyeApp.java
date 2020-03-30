a262e7875716cfca4bbe921c0b5f04e5a2c74ab9
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2020-03-30 15:16:40 +0100

    Added more messages to .proto file

--- eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
+++ eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
@@ -1,5 +1,8 @@
 package pt.tecnico.sauron.eye;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class EyeApp {
 
@@ -10,6 +13,23 @@ public class EyeApp {
 		System.out.printf("Received %d arguments%n", args.length);
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+		handleInput();
+
+		System.out.println("End");
+	}
+
+	public static void handleInput() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (true) {
+				String line = scanner.nextLine();
+
+				System.out.println(line);
+			}
+		}
+		catch (NoSuchElementException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 	
