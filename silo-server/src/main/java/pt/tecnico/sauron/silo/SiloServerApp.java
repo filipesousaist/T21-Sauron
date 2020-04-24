c5ffad9cc36762b609f84fde4063a7889681a46c
Deejay-Vitelio <pedro.vilela@tecnico.ulisboa.pt>
2020-04-24 20:58:16 +0100

    Começo da implementação do serviço de nomes

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerApp.java
@@ -4,6 +4,7 @@ package pt.tecnico.sauron.silo;
 import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import pt.ulisboa.tecnico.sdis.zk.ZKNaming;
 
 import java.io.IOException;
 import java.sql.Timestamp;
@@ -13,11 +14,16 @@ import java.util.Date;
 public class SiloServerApp {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		int port = Integer.parseInt(args[0]);
+		String zkhost = args[0];
+		int zkport = Integer.parseInt(args[1]);
+		int instance = Integer.parseInt(args[2]);
+		String serverHost = args[3];
+		int serverPort = Integer.parseInt(args[4]);
 
+		ZKNaming zkNaming = null;
 		BindableService service = new SiloServerImpl();
 
-		Server server = ServerBuilder.forPort(port).addService(service).build();
+		Server server = ServerBuilder.forPort(serverPort).addService(service).build();
 
 		server.start();
 
